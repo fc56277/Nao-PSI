dd49de0497c7e331eb58becbc257954c4d295f16
Joao Gonçalo Santos <fc57103@alunos.fc.ul.pt>
2023-04-24 20:33:24 +0100

    Implementado redirecionamento por definição para /login. Link ./userProfile também reencaminha para o devido .html do componente em questão.

--- angular-loja-online/src/app/app-routing.module.ts
+++ angular-loja-online/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {UserLoginComponent} from "./user-login/user-login.component"
+import {UserProfileComponent} from "./user-profile/user-profile.component"
 
 const routes: Routes = [
-    { path: '/', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: UserLoginComponent }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: UserLoginComponent },
+    { path: 'userProfile', component: UserProfileComponent }
 ];
 
 @NgModule({
