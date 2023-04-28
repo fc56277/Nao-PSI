f4fdc19a1707321a6fd127dfd164aa63ed1fc1b2
fc56277 <fc56277@alunos.fc.ul.pt>
2023-04-28 11:58:16 +0100

    Adicionado logout e verificacao de login no express

--- express-loja-online/controllers/userController.js
+++ express-loja-online/controllers/userController.js
@@ -13,6 +13,17 @@ exports.user_login_get = async (req, res) => {
     res.status(200).json({ message: 'login bem sucedido' });
 };
 
+// verifica se o login foi efetuado
+exports.user_isLogged_get = async (req, res) => {
+  res.status(200).json({ value: req.session.user_id == undefined });
+};
+
+// faz logout do user atual
+exports.user_logout_get = async (req, res) => {
+  delete req.session.user_id;
+  res.status(200).json({ message: 'logout bem sucedido' });
+}
+
 // regista um novo user
 exports.user_register_post = async(req, res) => {
     const user = new User({ name: req.body.name, email: req.body.email, password:req.body.password, wishList: req.body.wishList });
