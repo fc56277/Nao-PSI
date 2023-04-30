f52ab51b5d6c226109d046ad2381148cf7f11c19
joaorsmatos02 <joao-matos02@hotmail.com>
2023-04-30 16:25:58 +0100

    adicionadas animacoes de transicao e corrigida borda branca

--- frontend-loja-online/src/app/user-register/user-register.component.ts
+++ frontend-loja-online/src/app/user-register/user-register.component.ts
@@ -3,11 +3,24 @@ import { User } from "../user";
 import { Game } from "../game";
 import { UserService } from "../user.service"
 import { Router } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-register.component.css']
+  styleUrls: ['./user-register.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s ease-in-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.5s ease-in-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class UserRegisterComponent {
     users: User[] = [];
@@ -28,7 +41,7 @@ export class UserRegisterComponent {
     }
 
     register(name: string, pass1: string, pass2: string): void {
-        const alphanumeric = /^[0-9a-zA-Z]+$/
+        const alphanumeric = /^[0-9a-zA-Z]+$/;
         const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]+$/;
 
         if(name.length < 3 || !alphanumeric.test(name)) {
@@ -61,8 +74,12 @@ export class UserRegisterComponent {
                 () => {
                     alert("Registo bem sucedido")
                     // Redirect to login page
-                    this.router.navigate(['/login']);
+                    this.goToLogin();
                 }
             );
-    }   
-}
+    }  
+    
+    goToLogin(): void {
+        this.router.navigate(['/login']);
+    }
+ }
